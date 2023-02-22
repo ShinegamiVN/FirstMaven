bd9677ae3ad08bf292437853c4a5e3ad1bb97709
ShinegamiVN <v.podliesnyi@wpt.dev>
2023-02-22 12:37:31 +0200

    Maven 1

---
+++ src/test/java/MavenClass.java
@@ -0,0 +1,30 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+import static java.time.Duration.ofSeconds;
+
+public class MavenClass {
+
+    public static WebDriver  driver;
+    public static void main(String[] args) {
+
+        //Setup Chrome driver
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+    }
+}
